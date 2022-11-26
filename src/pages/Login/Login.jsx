2cdf8cbb7d20420e86f3a57bef31d417c0cd37dd
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2022-11-26 17:48:23 -0300

    corrigiu texto do h1
    corrigiu texto do h1 da tela de login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,7 @@
 export const Login = () => {
     return (
     <div className="Login">
-      <h1>Voce estar na página de login</h1>
+      <h1>Você está na página de login</h1>
     </div>
   )
 }
