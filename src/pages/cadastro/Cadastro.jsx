0e86446f9555372d0e31215f3a3416e4930a1635
jGledsonSilva <gledson.silva60@aluno.ifce.edu.br>
2022-11-25 22:18:33 -0300

    Adicionando botão - MUI

--- src/pages/cadastro/Cadastro.jsx
+++ src/pages/cadastro/Cadastro.jsx
@@ -1,8 +1,10 @@
+import { Button } from "@mui/material"
 
 export const Cadastro = () => {
     return (
         <div>
             <h1>Voce estar na página de cadastro</h1>
+            <Button variant="contained" size="large" href="pagina-inicial">Voltar para a página inicial</Button>
         </div>
     )
 }
