58c17110230eae70ba423283057b66a2ed51d719
jGledsonSilva <gledson.silva60@aluno.ifce.edu.br>
2022-11-27 14:15:00 -0300

    mudando estilo do botão e acesso a rotas

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,13 @@
 import { Button } from "@mui/material"
-import { useNavigate } from "react-router-dom";
-
 
 export const Home = () => {
-const history = useNavigate();
-
-const handleClick = () => {
-  history('/cadastrar')
-}
-
-    return (
-      <div className="Home">
-        <h1>Voce estar na página inicial</h1>
-        <Button onClick={handleClick}>cadastra-se</Button>
-      </div>
+  return (
+    <div className="Home">
+          <h1>
+            Voce estar na página inicial
+          </h1>
+          <Button variant="contained" size="large" href="entrar">login</Button>
+    </div>
   )
 }
 
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,12 @@
+import { Button } from "@mui/material"
+
+
 export const Login = () => {
     return (
     <div className="Login">
       <h1>Voce estar na página de login</h1>
+      <Button variant="contained" size="large"href="cadastrar">cadastra-se</Button>
+      <Button variant="contained" size="large" href="pagina-inicial">Voltar para a página inicial</Button>
     </div>
   )
 }
