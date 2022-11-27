f7a63c5f523ae8b7ab5848b4605c450bfdb6a3e5
jGledsonSilva <gledson.silva60@aluno.ifce.edu.br>
2022-11-27 16:47:10 -0300

    mudando o estilo do botão e acesso a rotas

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material"
+
+export const Home = () => {
+  return (
+    <div className="Home">
+          <h1>
+            Voce estar na página inicial
+          </h1>
+          <Button variant="contained" size="large" href="entrar">login</Button>
+    </div>
+  )
+}
+
+export default Home
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,28 +1,14 @@
 import { Button } from "@mui/material"
 
-export const Home = () => {
-<<<<<<< HEAD
-  return (
-    <div className="Home">
-          <h1>
-            Voce estar na página inicial
-          </h1>
-          <Button variant="contained" size="large" href="entrar">login</Button>
-    </div>
-=======
-const history = useNavigate();
 
-const handleClick = () => {
-  history('/cadastrar')
-}
-
+export const Login = () => {
     return (
-      <div className="Home">
-        <h1>Você está na página inicial</h1>
-        <Button onClick={handleClick}>Cadastra-se</Button>
-      </div>
->>>>>>> b3b3a2d9e5237d38af78048aaefd5afaeaed23a2
+    <div className="Login">
+      <h1>Voce estar na página de login</h1>
+      <Button variant="contained" size="large"href="cadastrar">cadastra-se</Button>
+      <Button variant="contained" size="large" href="pagina-inicial">Voltar para a página inicial</Button>
+    </div>
   )
 }
 
-export default Home
+export default Login
