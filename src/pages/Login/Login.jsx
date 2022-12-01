aeb26e42742cc71a3810f4e1268c6de27b1cffec
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2022-12-01 17:05:27 -0300

    feat(1): adicionei o form e o texto lateral

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,35 +1,39 @@
 import { TextField, Button, Link } from '@mui/material';
+import './styles.css';
+
+let nome;
+let senha;
 
 export const Login = () => {
   return (
-    <div className="Login">
-      <h1>Você está na página de login</h1>
-      <div className="FormularioLogin">
+    <div id='login'>
+      
+      <div className='sobre'>
+        <h2>Sobre</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam saepe ratione inventore culpa quas nobis molestias? Deleniti atque vero nostrum recusandae beatae ducimus! Deleniti, expedita ut. Cupiditate non inventore cum!</p>
+        <h2>Pomodoro</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae magnam nesciunt id, fugiat soluta, repellat explicabo ad repudiandae illum suscipit quidem nihil error. Obcaecati voluptatem nulla provident asperiores ipsam est.</p>
+        <h2>Outro</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam error necessitatibus voluptates at exercitationem tempore fugiat ratione labore vero possimus quibusdam molestiae, totam sapiente rem, reprehenderit provident pariatur consequatur vitae?</p>
+      </div>
+
+      <div id="formularioLogin">
         <h1>Login</h1>
-        <Button onClick={handleClick}>Entrar com o Google</Button>
+        <Button variant="contained" size="large" href="#">Entrar com o Google</Button>
+        <span>ou</span>
         <TextField
           id="outlined-name"
           label="Usuário ou email" 
           value={nome}
-          onChange={handleChange}
         />
         <TextField
           id="outlined-name"
           label="Senha"
           value={senha}
-          onChange={handleChange}
         />
-        <Button onClick={handleClick}>Entrar</Button>
-        <p>Ainda não tem uma conta?</p>
-        <Link
-          component="button"
-          variant="body2"
-          onClick={() => {
-            console.info("Clicou em cadastrar");
-          }}
-        >
-          Cadastre-se
-        </Link>
+        <Button variant="contained" size="large" href="#">Entrar</Button>
+        <span>Ainda não tem uma conta?</span>
+        <a href="#">Cadastre-se</a>
       </div>
     </div>
   )
