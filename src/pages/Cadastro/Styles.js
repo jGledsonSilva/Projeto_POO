22e468dc921bee1909e737173bda34d61f927ef2
jGledsonSilva <gledson.silva60@aluno.ifce.edu.br>
2022-12-09 16:21:32 -0300

    feature: add styles

---
+++ src/pages/Cadastro/Styles.js
@@ -0,0 +1,17 @@
+const teste = {
+    backgroundColor: "#141414",
+    height: "100vh",
+}
+
+const pap = {
+    width: "750px",
+    height: "420px",
+    padding: 5,
+    backgroundColor: "#d9d9d9",
+    margin: 5
+}
+
+export const Styles = {
+    teste,
+    pap
+}
