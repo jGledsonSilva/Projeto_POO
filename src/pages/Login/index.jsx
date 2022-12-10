f1dbca448d68d4bdd4deabe15cde80af411ab3dd
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2022-12-10 17:40:51 -0300

    feat(1): conteudo e layout base

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,4 +1,5 @@
 import { TextField, Button } from '@mui/material';
+import { BaseLayout } from "../../shared/components/layouts/BaseLayout";
 import './styles.css';
 
 let nome;
@@ -6,44 +7,50 @@ let senha;
 
 export const Login = () => {
   return (
-    <div id='login'>
+    <BaseLayout>
 
-      <div className='sobre'>
-        <div>
-          <h2>Sobre</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam saepe ratione inventore culpa quas nobis molestias? Deleniti atque vero nostrum recusandae beatae ducimus! Deleniti, expedita ut. Cupiditate non inventore cum!</p>
+      <div id='login'>
+
+        <div className='sobre'>
+          <div>
+            <h2>HealthPomo</h2>
+            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+          </div>
+
+          <div>
+            <h2>Pomodoro</h2>
+            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+          </div>
+
+          <div>
+            <h2>Sobre</h2>
+            <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+          </div>
         </div>
 
-        <div>
-          <h2>Pomodoro</h2>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae magnam nesciunt id, fugiat soluta, repellat explicabo ad repudiandae illum suscipit quidem nihil error. Obcaecati voluptatem nulla provident asperiores ipsam est.</p>
-        </div>
-
-        <div>
-          <h2>Outro</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam error necessitatibus voluptates at exercitationem tempore fugiat ratione labore vero possimus quibusdam molestiae, totam sapiente rem, reprehenderit provident pariatur consequatur vitae?</p>
+        <div id="formularioLogin">
+          <h1>Login</h1>
+          <Button variant="contained" size="large" href="#">Entrar com o Google</Button>
+          <span>ou</span>
+          <TextField
+            id="outlined-name"
+            label="Usuário ou email"
+            value={nome}
+          />
+          <TextField
+            id="outlined-name"
+            label="Senha"
+            value={senha}
+          />
+          <Button variant="contained" size="large" href="pagina-inicial">Entrar</Button>
+          <div className='cadastro'>
+            <span>Não tem uma conta?</span>
+            <a href="cadastrar">Cadastre-se</a>
+          </div>
         </div>
       </div>
-
-      <div id="formularioLogin">
-        <h1>Login</h1>
-        <Button variant="contained" size="large" href="#">Entrar com o Google</Button>
-        <span>ou</span>
-        <TextField
-          id="outlined-name"
-          label="Usuário ou email"
-          value={nome}
-        />
-        <TextField
-          id="outlined-name"
-          label="Senha"
-          value={senha}
-        />
-        <Button variant="contained" size="large" href="#">Entrar</Button>
-        <span>Ainda não tem uma conta?</span>
-        <a href="#">Cadastre-se</a>
-      </div>
-    </div>
+    </BaseLayout>
+    
   )
 }
 
