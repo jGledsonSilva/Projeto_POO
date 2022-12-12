d5346ccde478d5c18d842b00892d128359bc093f
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2022-12-11 21:28:38 -0300

    feature(1): adicionando btn login no baselayout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,19 +5,10 @@ import { BaseLayout } from "../../shared/components/layouts/BaseLayout";
 export const Home = () => {
   const history = useNavigate();
 
-  const cadastrar = () => {
-    history('/cadastrar')
-  }
-  const login = () => {
-    history('/login')
-  }
-
   return (
     <BaseLayout>
       <div className="Home">
         <h1>Você está na página inicial</h1>
-        <Button onClick={cadastrar}>Cadastra-se</Button>
-        <Button onClick={login}>Fazer login</Button>
       </div>
     </BaseLayout>
   )
--- src/shared/components/layouts/BaseLayout/index.jsx
+++ src/shared/components/layouts/BaseLayout/index.jsx
@@ -1,4 +1,5 @@
-import { Box, Typography } from "@mui/material";
+import { Home } from "@mui/icons-material";
+import { Box, Typography, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Logo from "../../../../assets/Logo.svg";
 import { styles } from "./styles";
@@ -6,14 +7,26 @@ import { styles } from "./styles";
 export const BaseLayout = ({ children }) => {
   const navigate = useNavigate();
 
+  const login = () => {
+    navigate('/login')
+  }
+  const home = () => {
+    navigate('/pagina-inicial')
+  }
+
   return (
     <Box id="body">
       <Box id="header" sx={styles.headerBackground}>
-        <Box id="header-logo" sx={styles.bodySpacing}>
-          <img src={Logo} width="24px"/>
+        <Box onClick={home} id="header-logo" sx={styles.bodySpacing}>
+          <img src={Logo} width="24px" />
           <Typography component="span" sx={styles.logoTitle}>
             <Typography component="span" sx={styles.titleColor}>Health</Typography>Pomo
           </Typography>
+        </Box>
+        <Box id="end">
+          <Box id="buttons">
+            <Button variant="contained" size='small' onClick={login}>Fazer login</Button>
+          </Box>
         </Box>
       </Box>
       <Box id="body-background" sx={styles.bodyBackground}>
