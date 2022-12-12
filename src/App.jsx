f558595dbc456041c78543fd93fd71c365bb71f5
SaldanhaElisson <elissonsaldanha19@gmail.com>
2022-12-11 23:25:09 -0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { CountDownProvider } from "./shared/contexts/CountDown";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./shared/themes/test";
 
+import { AppRoutes } from "./routes/index"
+
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
