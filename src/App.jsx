3e48ec1801a2d1fd4eadde0788033e21e70a267b
Renêr <91918580+renercrisostomo@users.noreply.github.com>
2022-12-11 23:14:55 -0300

    fix: approutes is not defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CountDownProvider } from "./shared/contexts/CountDown";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./shared/themes/test";
+import { AppRoutes } from "./routes/AppRoutes";
 
 export const App = () => {
   return (
